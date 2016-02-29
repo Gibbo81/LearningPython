9834ced8de9bb86812b735c21b24cb94781affab
Giuliano Masi <masigiuliano@yahoo.it>
2016-02-29 22:21:44 +0100

    BlockString

--- PythonApplication1/07StringFundamentals.py
+++ PythonApplication1/07StringFundamentals.py
@@ -6,7 +6,22 @@ def Concatenation():
     print("len(s): " + str(len(s)))  #7: 4 characters(a,b,c,h) + newline \n + 2 tab \t
     return;
 
-
+def BlockString():          #""" .....      """
+    print("BlockString: ''' dall'alba al.....        tramonto'''")
+    st= """In the future 
+                            there is
+                                            only war"""
+    u='''normale'''
+    print
+    print(st)
+    #multiline comment
+    '''
+    b=a
+    import sys
+    print(sys.getrefcount(b))
+    print(sys.getrefcount(a))
+    '''
+    return;
 
 
 
@@ -20,7 +35,7 @@ s1= 'c:\new\file.txt'
 print(r"r'c:\new\file.txt : '" +s)
 print(r'c:\new\file.txt : ' +s1)
 print('------------------------------------------------------------')
-
+BlockString()
 print('------------------------------------------------------------')
 
 print('------------------------------------------------------------')
