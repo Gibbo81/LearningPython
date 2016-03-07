52fbfb28b15668ee691243bd088795bb970e1bd8
Giuliano Masi <masigiuliano@yahoo.it>
2016-03-07 20:11:07 +0100

    dictionaries

--- PythonApplication1/08Lists and Dictionaries.py
+++ PythonApplication1/08Lists and Dictionaries.py
@@ -40,24 +40,51 @@ def InPlaceChange():
     print(lis)      
     lis[1:3]=[-999,-998, -997, -996]    # Slice assignment: change a block with slice assignment--> first a deletion and then a new insertion
     print(lis)
-    lis[1:2]=[]                           # this can be considered a deletion
+    lis[1:2]=[]                           # this can be considered a deletion but only work on slice assignment
+    lis[2]=[]                             # this only assi to position [2] a reference to an empty object   
+    del lis[1]                            # questo cancella veramente la posizione [1]
     lis[1:1]=[44,45]                      # and this an insertion
     print(lis)
     
-    lis.append('append a single object')
-    lis.extend(["take a list",8,4,0])
+    lis.append('append a single object')    #add an object
+    lis.extend(["take a list",8,4,0])       #add a list of object
     print(lis)
+    lis.insert(1,'posizione uno [1]')       # add the object in the specific position
+    print(lis)
+    return;
+
+def ListMethod():
+    li=[1,2,1,13,4,'appo']
+    print(li.index(1))
+    print(li.count(1))    
+    return;
+
+def BasicDictionaryOperator():
+    print("start with dictionary")
+    di={'a':1,"b":2,'c':3, 4:'quattro'}
+    print(di)
+    print("di['c'] : "+ str(di['c']))
+    print("di[4] : "+ str(di[4]))
+    di={'a':1,'b':2,'c':3, 'd':{"zz":12,'vv':34}}
+    print(di)
+    print('di["d"]["vv"] : ' + str(di["d"]["vv"]))
+    print("len(di) : " + str(len(di)))
+    keys=di.keys()                                  # type is dict_keys
+    print(keys)
+    print(list(keys))                               # if we want a list we should uselist(...) 
+    print('"b" in di : ' + str("b" in di))          #check if a key with this value is present into the dictionary
+    print('"y" in di : ' + str("y" in di))
     return;
 
 BasicListOperations()
 print('------------------------------------------------------------')
 Indexing()
 print('------------------------------------------------------------')
-InPlaceChange();
+InPlaceChange()
 print('------------------------------------------------------------')
-
+ListMethod()
 print('------------------------------------------------------------')
-
+BasicDictionaryOperator()
 print('------------------------------------------------------------')
 
 print('------------------------------------------------------------')
