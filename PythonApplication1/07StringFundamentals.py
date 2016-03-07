0bd3e1718d21d330c89b7614d3e81e0a9be8b5eb
Giuliano Masi <masigiuliano@yahoo.it>
2016-03-07 08:17:27 +0100

    refactoring

--- PythonApplication1/07StringFundamentals.py
+++ PythonApplication1/07StringFundamentals.py
@@ -69,6 +69,20 @@ def FormattingExpression():
     print('All whith \%: ' +  '%s %s %s you' % (1, 'spam', 4.0))  
     return;
 
+def DictinaryBaseFormattingExpression():
+    dict={"qty":1, "b":2,"c":3,"d":4}   
+    str="prova %(qty)d inserisce - %(d)s"   
+    print(str % dict )          # inserisci il valore del dizionario che ha come chiave d trattandolo con il passaggio %s (conversione a stringa)
+    return;
+
+def StringFormattingMethod():
+    template = 'Inseriamone tre: {0}, {1}, {2}'     #by position
+    template2 = 'Inseriamone tre: {}, {}, {}'       #by relative position
+    print(template.format('un',"dos",'tres'))
+    print(template2.format('un',"dos",'tres'))
+    return;
+
+
 Concatenation()
 print('------------------------------------------------------------')
 print('raw string')
@@ -87,8 +101,7 @@ FormattingExpression()             #format method
 print('------------------------------------------------------------')
 CommonUsedMethod()
 print('------------------------------------------------------------')
-
 print('------------------------------------------------------------')
-
+DictinaryBaseFormattingExpression()
 print('------------------------------------------------------------')
-
+StringFormattingMethod()
