3ed8b7af46ed8768bbc6f7a58a1ac04dcdb3d8dd
Giuliano Masi <masigiuliano@yahoo.it>
2016-03-15 22:37:23 +0100

    binary files

--- PythonApplication1/09Tuples, Files, and Everything Else.py
+++ PythonApplication1/09Tuples, Files, and Everything Else.py
@@ -20,9 +20,6 @@ def UsingFiles():
     CreateAFile()
     print('*****') 
     ReadAFile()
-
-
-
     return;
 
 def CreateAFile():
@@ -37,7 +34,6 @@ def ReadAFile():
     print('leggo il file: myfile.txt') 
     for line in open(r".\09files\myfile.txt"):     #r is the default
         print(line,end='')
-
     print('Read again with different command') 
     readfile2=open(r".\09files\myfile.txt") 
     print(readfile2.readline(),end='')
@@ -46,8 +42,43 @@ def ReadAFile():
     readfile2.close()
     return;
 
+def BinaryFiles():
+    print('leggo il file come binario') 
+    data = open(r".\09files\02-Audio20CD1.ico", 'rb').read()     #rb is read binary
+    print(data)
+    open(r".\09files\copy02.cop", 'wb').write(data)   
+    return;
+
+def SaveObjectonFile():
+    x,y=33,88.45
+    s='pippo'
+    dic={'a':"pluto",'b':4}
+    li=[1,3,8,90,"rty"]
+    file = open('.\\09files\\insertobjects.txt',"w")  
+    file.write(s + '\n')
+    file.write('%s,%s \n' % (x,y))              #two diferent ways to have a conversion between files
+    file.write(str(dic) + ' | ' + str(li)+ "\n")
+    file.close()
+
+    filer=open('.\\09files\\insertobjects.txt')
+    string=filer.readline()
+    string=string.rstrip()
+    print(string)
+    number=filer.readline()
+    splitted=number.split(',')
+    print(int(splitted[0]))
+    print(float(splitted[1]))
+    splitted = filer.readline().rstrip().split('|')
+    dictionary = eval(splitted[0])      #EVAL a build-in function that treat a string as a python expression
+    list = eval(splitted[1])            #it's the same of writing  list= [...]  con l'assegnamento che è fatto dal contenuto della stringa
+    print (str(list) + ' ' + str(dictionary))
+    filer.close()
+    return;
+
+
+
 print((40))
-x=(40,)             #tu have a list of one element(useless)
+x=(40,)             #to have a list of one element(useless)
 print(x)
 print('------------------------------------------------------------')
 TupleAreImmutable()
@@ -56,9 +87,9 @@ TupleMethod()
 print('------------------------------------------------------------')
 UsingFiles()
 print('------------------------------------------------------------')
-
+BinaryFiles()
 print('------------------------------------------------------------')
-
+SaveObjectonFile()
 print('------------------------------------------------------------')
 
 print('------------------------------------------------------------')
