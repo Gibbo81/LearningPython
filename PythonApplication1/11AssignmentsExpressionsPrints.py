f94ce6810059d3926eaf6ff707cd098a39e15116
Giuliano Masi <masigiuliano@yahoo.it>
2016-03-22 22:32:20 +0100

    11 assignments

--- PythonApplication1/11AssignmentsExpressionsPrints.py
+++ PythonApplication1/11AssignmentsExpressionsPrints.py
@@ -3,7 +3,7 @@ def VariableSwitch(a,b):       #swithc 2 variable without using a temporary vari
     return a,b;
 
 def UnpackingAssignment():
-    a,b=10,5
+    a,b=10,5                    #Sequence Assignment
     print(a,b)
     a,b=VariableSwitch(a,b)
     print(a,b)
@@ -14,12 +14,22 @@ def PythonEnumerator():
     print(a,b,c)
     return;
 
+def SequenceAssignment():
+    seq=[1,2,3,4,5]
+    a,b,c,d,e=seq           #Sequence Assignment
+    print(a,b,c,d,e)
+    a,*b,c=seq                #a match the first name in the sequence, but b match everithing after that
+    print("a,*b,c=seq : " + str(a)+','+str(b)+','+str(c)) #With * items number on the left and on the laft do not have tomatch 
+    while seq:
+        print(seq)
+        left,*seq=seq
+    return;
 
 UnpackingAssignment()
 print('------------------------------------------------------------')
 PythonEnumerator()
 print('------------------------------------------------------------')
-
+SequenceAssignment()
 print('------------------------------------------------------------')
 
 print('------------------------------------------------------------')
