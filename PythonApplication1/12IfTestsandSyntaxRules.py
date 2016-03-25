d6904f412cf04f0a1ae20b879e45b17bbad8768d
Giuliano Masi <masigiuliano@yahoo.it>
2016-03-25 22:23:55 +0100

    Starting chapter 12

---
+++ PythonApplication1/12IfTestsandSyntaxRules.py
@@ -0,0 +1,40 @@
+def TestingIfSequence(value):
+    if value<10:            #if,elif and else must line up vertically with the same indentation
+        print(str(value) + " its' less than 10")
+    elif value<20:
+        print(str(value) + " its' less than 20")
+    elif value<30:
+        print(str(value) + " its' less than 30")
+    elif value<40:
+        print(str(value) + " its' less than 40")
+    elif value<50:
+        print(str(value) + " its' less than 50")
+    elif value<60:
+        print(str(value) + " its' less than 60")
+    else:
+        print(str(value) + " its' >= 60")
+    return;
+
+
+print('------------------------------------------------------------')
+TestingIfSequence(0)
+TestingIfSequence(25)
+TestingIfSequence(50)
+TestingIfSequence(159)
+print('------------------------------------------------------------')
+
+print('------------------------------------------------------------')
+
+print('------------------------------------------------------------')
+
+print('------------------------------------------------------------')
+
+print('------------------------------------------------------------')
+
+print('------------------------------------------------------------')
+
+print('------------------------------------------------------------')
+
+print('------------------------------------------------------------')
+
+print('------------------------------------------------------------')
