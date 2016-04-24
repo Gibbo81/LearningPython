34df80f2df3b631f10a04c98ff218653532fbf46
Giuliano Masi <masigiuliano@yahoo.it>
2016-04-24 20:37:05 +0200

    completed chapter 20

--- PythonApplication1/20ComprehensionsAndGenerations.py
+++ PythonApplication1/20ComprehensionsAndGenerations.py
@@ -41,9 +41,12 @@ def DoublweForStatment():     #work with matrix
     matrix=[x+10 for row in matrix for x in row]   #double for statmnet 
     print('A single list Matrix: ',matrix)
 
-def GeneratorFunction(k):   #it returns an iteretor non all the set of result
+def GeneratorFunction(k):   #Returns an iteretor non all the set of result
     for x in range(k):      #when yield is hit it return a single result and then stop the funtion
         yield x             #it will start again when asked for a new result
+
+def YieldFrom(k):
+    yield from range(k) #-->for i in range(k): yield i it's the same
 
 def GeneratorFunctionWithReturn():
     while True:
@@ -53,15 +56,42 @@ def GeneratorFunctionWithReturn():
             return      #return stopped the generation of new value and exit definitively from the function
         yield k
 
-def Yieldnext():
+def YieldSend():
     for i in range(10): #f.send(35)
         x= yield(1)     #using the send command it's possible to send back some values to the
         print(x)        #generator function
 
 def GeneratorExpression(n):
     ge= (x**n for x in range(10))   #similar to list comprehension but jit calculation
+    print(ge)
     for i in ge:                    #the difference are () instead of []
         print(i)                    #return an iterator instead of a list of result
+
+def permute1(seq):
+	if not seq: 
+		return [seq] 
+	else:
+		res = []
+		for i in range(len(seq)):
+			rest = seq[:i] + seq[i+1:] #removed current node and then recursive call
+			for x in permute1(rest): 
+			    res.append(seq[i:i+1] + x) 
+		return res
+
+def permute2(seq):  #using generation function we do not havceto wait for all the data to 
+	if not seq:     #be calculated. We give back the result while we get it
+		yield [seq] 
+	else:
+		for i in range(len(seq)):
+			rest = seq[:i] + seq[i+1:] 
+			for x in permute1(rest): 
+			    yield seq[i:i+1] + x 
+
+def SetandDictionaryComprehension(n):
+    set={x*x for x in range(n)}
+    dictionary={x : x*x for x in range(n)}
+    return set,dictionary
+
 
 print('-----------------------------------------------------------------')
 print("Now we have 3 ways to apply an expression to all items inside an iterable")
@@ -87,7 +117,7 @@ for rs in GeneratorFunctionWithReturn():
     print(rs)
 print('-----------------------------------------------------------------')
 print('Generator function: using send')
-f=Yieldnext()
+f=YieldSend()
 next(f) #start generator necessry before the use of send(x)
 print('f.send(88) : ',f.send(88))
 print("f.send(35) :",f.send(35))
@@ -96,9 +126,21 @@ print('-----------------------------------------------------------------')
 print("Generator Expression")
 GeneratorExpression(3)
 print('-----------------------------------------------------------------')
-
+print("Generator expression and generator function are single iteration object")    #single iteration object
+print('They are their own iterator so a call at iter(...) is useless')
+res=GeneratorFunction(9)
+print("iter(res) is res :",iter(res) is res)
+seconditerator=iter(res)
+print(next(res))
+print(next(seconditerator)) #this one start from where the first left
+print(next(res))
 print('-----------------------------------------------------------------')
-
+print(permute1([0,1,2,3,4,5]))
+for x in permute2([11,22,33,44,55,77]):
+    print(x, end=' *** ')
 print('-----------------------------------------------------------------')
-
+print('We can also use dictionary and set comprehension')
+r1,r2=SetandDictionaryComprehension(5)
+print(r1)
+print(r2)
 print('-----------------------------------------------------------------')
