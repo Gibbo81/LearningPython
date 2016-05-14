041da9375b130dfdc1307c633095dfb23835fda1
Giuliano Masi <masigiuliano@yahoo.it>
2016-05-14 17:01:22 +0200

    Starting chapter 25

--- PythonApplication1/importing/Changer.py
+++ PythonApplication1/importing/Changer.py
@@ -1,11 +1,13 @@
 print('Importing Changer')
 X=100
 print("Done x:100")
+print("printing variable __name__:", __name__)
 
-
-
-
-
+#We can create a sort a unit test that do not run when we are importing the module!!!!!
+if __name__=='__Main__':                        
+    print("used as main for the program")
+else:
+    print("imported as module")
 
 
 
