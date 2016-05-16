994d0d0a3b5f152d7e376943a150b0fd1dccad70
Giuliano Masi <masigiuliano@yahoo.it>
2016-05-16 22:42:04 +0200

    Working on chapter 25

--- PythonApplication1/23ModuleCodingBasics.py
+++ PythonApplication1/23ModuleCodingBasics.py
@@ -6,10 +6,11 @@ print('if we run the iport again nothing happen')
 import importing.PrintOnImport
 #there is no link from a name copied with from back to the file it came from. To really change a global name in
 #another file, you must use import:  import small; small.x = 42 # Changes x in other module
-#from only copy names from one module to another
+#from only copy names from one module to another, we get copies of those names not links to them
 
+print("from only copy names from one module to another, we get copies of those names not links to them")
 print('-----------------------------------------------------------------')
-print("list(importing.PrintOnImport.__dict__.keys()) : ",list(importing.PrintOnImport.__dict__.keys()));
+print("list(importing.PrintOnImport.__dict__.keys()) : ",list(importing.PrintOnImport.__dict__.keys()));    #__dict__ dictionary with all the attributeof a module
 userdefinedlist=[name for name in importing.PrintOnImport.__dict__.keys() if not name.startswith('__')]
 print("Print only name define in the code: ",userdefinedlist)
 print('-----------------------------------------------------------------')
--- PythonApplication1/25AdvancedModulesTopic.py
+++ PythonApplication1/25AdvancedModulesTopic.py
@@ -9,7 +9,15 @@ def TestingCommandLineParameter():
             print(x, end=' ')
         print("")
 
-
+def DynamicImportExec(importingModule): #import the module importingModule in a dynamic way
+    print('To import a dinamically function we can use the exec statment')
+    exec('import '+importingModule + ' as pip')
+    
+def DynamicImportimportlib(importingModule):    #really better
+        print('To import a dinamically function we can use the importlib library')
+        import importlib
+        imported = importlib.import_module(importingModule)   
+        print(imported)
 
 
 print('-----------------------------------------------------------------')
@@ -20,7 +28,11 @@ print('in an imported module __name__ is the name of the file: importing.Changer
 print('-----------------------------------------------------------------')
 TestingCommandLineParameter()
 print('-----------------------------------------------------------------')
-
+import sys
+sys.getprofile
+DynamicImportExec("importing.ImportVariable")
+print('********************')
+DynamicImportimportlib("importing.PrintOnImport")
 print('-----------------------------------------------------------------')
 
 print('-----------------------------------------------------------------')
@@ -35,8 +47,8 @@ print('-----------------------------------------------------------------')
 
 print('-----------------------------------------------------------------')
 
-print('-----------------------------------------------------------------')
 
-print('-----------------------------------------------------------------')
+
+
 
 
