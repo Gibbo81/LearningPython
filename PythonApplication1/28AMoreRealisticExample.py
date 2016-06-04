6e1eb2ccc06cd27237aef12660230396c874a588
Giuliano Masi <masigiuliano@yahoo.it>
2016-06-04 12:09:46 +0200

    Completed chapter 28

--- PythonApplication1/28AMoreRealisticExample.py
+++ PythonApplication1/28AMoreRealisticExample.py
@@ -1,4 +1,14 @@
-import person
+ï»¿import person
+import shelve
+
+def ReadFromShelve(path):
+    reader=shelve.open(path)
+    print('number of records: ',len(reader))
+    
+    for key in reader:    #same as: for x in reader.keys():  
+        print(reader[key])
+    reader.close()
+
 
 
 
@@ -9,11 +19,36 @@ print("bob.__class__.__name__: ", bob.__class__.__name__)
 boss= person.Manager("TheOne",100)
 print("boss.__class__.__bases__: ", boss.__class__.__bases__)   #all the superclass
 print('--------------------------------------------')
+print('Storing object in a shelve DB')
+pippo= person.Person("Goofy",'Discontinuos',10)
+topolino=person.Person("Mouse",'Detective',44)
+basettoni=person.Manager("Seamus O'Hara",100)
 
+dbopened=shelve.open(".\\28files\\newshelvefile")
+for obj in (pippo,topolino,basettoni):
+    dbopened[obj.name]=obj
+    print("saved: ",obj.name)
+dbopened.close()
+print("Let's add more people")
+
+top=person.Person("Test",'--')
+top2=person.Person("Test2",'****',1)
+dbopened=shelve.open(".\\28files\\newshelvefile")
+for obj in (top,top2):
+    dbopened[obj.name]=obj      #this is the write command!
+    print("saved: ",obj.name)
+dbopened.close()
 print('********************************************')
-
+print('Now read data from our db files')
+ReadFromShelve(".\\28files\\newshelvefile")
 print('--------------------------------------------')
-
+print("Load change an object and save it back")
+rw=shelve.open(".\\28files\\newshelvefile")
+goofy=rw["Goofy"]
+goofy.PayRaise(9)
+rw["Goofy"]=goofy
+rw.close()
+ReadFromShelve(".\\28files\\newshelvefile")
 print('********************************************')
     
 print('--------------------------------------------')
@@ -24,11 +59,11 @@ print('********************************************')
 '''
 To minimize the chances of name collisions like this, Python programmers often prefix
 methods not meant for external use with a single underscore: _gatherAttrs in our case.
-This isn’t foolproof (what if another class defines _gatherAttrs, too?), but it’s usually
-sufficient, and it’s a common Python naming convention for methods internal to a class.
+This isnâ€™t foolproof (what if another class defines _gatherAttrs, too?), but itâ€™s usually
+sufficient, and itâ€™s a common Python naming convention for methods internal to a class.
 A better and less commonly used solution would be to use two underscores at the front
 of the method name only: __gatherAttrs for us. Python automatically expands such
-names to include the enclosing class’s name, which makes them truly unique when
+names to include the enclosing classâ€™s name, which makes them truly unique when
 looked up by the inheritance search.
 '''
 
