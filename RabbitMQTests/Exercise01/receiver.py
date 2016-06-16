8e6baa2cfc5886849e5ad743c8624c78cb48a07c
Giuliano Masi <masigiuliano@yahoo.it>
2016-06-16 12:46:15 +0200

    changed string rapresentation

--- RabbitMQTests/Exercise01/receiver.py
+++ RabbitMQTests/Exercise01/receiver.py
@@ -2,7 +2,9 @@ import pika
 
 def callback(ch, method, properties, body):
     print(str(body))
-    print('The payload of the message is : %s' % str(body))
+    print('The payload of the message is : %s' % body.decode('UTF-8'))  #you can decode a bytes into a str
+#str = '...' literals = a sequence of Unicode characters (UTF-16 or UTF-32, depending on how Python was compiled)
+#bytes = b'...' literals = a sequence of octets (integers between 0 and 255)
 
 connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
 channel = connection.channel()
