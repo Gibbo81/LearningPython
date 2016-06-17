5f16445d34355868fe569d7d6309d5393fbea138
Giuliano Masi <masigiuliano@yahoo.it>
2016-06-17 17:00:07 +0200

    starting '2 Work queues'

---
+++ RabbitMQTests/Exercise02/send.py
@@ -0,0 +1,27 @@
+﻿import pika
+
+connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+channel = connection.channel()
+print("Connection Created")
+
+channel.queue_declare(queue='task_queue')
+
+def BuildMessage(difficulty):
+    message=''
+    for x in range(0,difficulty):
+        message += '.'
+    return message
+
+def SendVariableComplexityMessage(difficulty):   
+    message = BuildMessage(difficulty)
+    result = channel.basic_publish(exchange = '',
+                                   routing_key = 'task_queue',
+                                   body = message,
+                                   properties = pika.BasicProperties(delivery_mode=2)) # make message persistent
+    if result:
+        print('task_queue')
+    else:
+        print("Error")
+
+if __name__ == '__main__':
+    SendVariableComplexityMessage(4)
