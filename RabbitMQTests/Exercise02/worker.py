9d8c476a6dcc1227ab35135547379e5669ec7efc
Giuliano Masi <masigiuliano@yahoo.it>
2016-06-20 09:37:59 +0200

    starting the receiver

--- RabbitMQTests/Exercise02/worker.py
+++ RabbitMQTests/Exercise02/worker.py
@@ -1,4 +1,20 @@
+﻿import pika
+import time
 
+def CallBack(ch, method, properties, body):
+    print("Start working on message")
+    time.sleep(body.decode('UTF-8').count('.'))        #its the same body.count(b'.')
+    print('Completed work on message %s' % body.decode('UTF-8')) 
+
+connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+channel = connection.channel()
+print("Connection Created")
+
+channel.queue_declare(queue='task_queue')
+channel.basic_consume(CallBack,queue='task_queue',no_ack=True)
+
+print("Starting consuming messages")
+channel.start_consuming()
 
 
 
