630faa9bfb0b6f25a5b3ea53c7f4190b444d3bec
Giuliano Masi <masigiuliano@yahoo.it>
2016-06-20 19:27:05 +0200

    Added ack to rabbit

--- RabbitMQTests/Exercise02/worker.py
+++ RabbitMQTests/Exercise02/worker.py
@@ -3,8 +3,9 @@ import time
 
 def CallBack(ch, method, properties, body):
     print("Start working on message")
-    time.sleep(body.decode('UTF-8').count('.'))        #its the same body.count(b'.')
-    print('Completed work on message %s' % body.decode('UTF-8')) 
+    time.sleep(body.decode('UTF-8').count('.'))         #its the same body.count(b'.')
+    print('Completed work on message %s' % body.decode('UTF-8'))
+    ch.basic_ack(delivery_tag = method.delivery_tag)    #ch --> channel
 
 def CreateConsumer(name):
     connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
@@ -12,7 +13,7 @@ def CreateConsumer(name):
     print("Connection Created")
 
     channel.queue_declare(queue='task_queue')
-    channel.basic_consume(CallBack,queue='task_queue',no_ack=True)
+    channel.basic_consume(CallBack,queue='task_queue')
 
     print("Starting consuming messages by %s" % name)
     channel.start_consuming()
