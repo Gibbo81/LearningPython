46f7033864c4a2f0e1c002bbe7d1831f882c6704
Giuliano Masi <masigiuliano@yahoo.it>
2016-06-25 20:18:10 +0200

    started chapter 31

---
+++ PythonApplication1/files31/Processor.py
@@ -0,0 +1,21 @@
+
+
+class Processor():
+    def __init__(self, reader, writer):
+        self.reader = reader
+        self.writer = writer
+
+    def process(self):
+        while True:
+            data=self.reader.readline()#.read()
+            if not data: 
+                break
+            data= self.Converter(data)
+            self.writer.write(data)
+    
+    def Converter(self, data):
+        assert false, 'converter must be defined'
+
+
+
+
