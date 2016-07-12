89bd48373d0d26da0647e827e6b843c692a72740
Giuliano Masi <masigiuliano@yahoo.it>
2016-07-12 22:52:17 +0200

    work on different inheritance search pattern

--- PythonApplication1/32AdvancedClassTopic.py
+++ PythonApplication1/32AdvancedClassTopic.py
@@ -5,13 +5,14 @@ class SearchingTest:
         return SearchingTest(self.Data + other)
 
 class Base: attr = 1
-
 class A(Base): pass
-
 class B(Base): attr = 2
-
 class C(A,B): pass
 
+class Base1: pass
+class A1(Base): pass
+class B1: pass
+class C1(A1,B1): pass
 
 print('------------------------------------------------------------')
 a= SearchingTest(10)
@@ -24,10 +25,11 @@ print('Diamond Inheritance tree')
 x = C()
 print(x.attr)                               #the new search pattern MRO{method resolution order} is: x C A B base
 print("The value is taken from B class")    #the old one (2.X) would be x C A Base B
-print("Class c new search pattern --> C.__mro__: ", C.__mro__)
-
-
+print("Class c new search pattern for diamond --> C.__mro__: ", C.__mro__)
 print('------------------------------------------------------------')
+#For nondiamonds, though, the search is still as it has always been to the top, and then to the right
+print("Class c1 OLD search pattern (NO diamond) --> C1.__mro__: ", C1.__mro__)
+
 
 print('------------------------------------------------------------')
 
