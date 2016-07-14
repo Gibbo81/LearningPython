33bbe4f797d3df0fb2db076e2f46653a044c9772
Giuliano Masi <masigiuliano@yahoo.it>
2016-07-14 11:05:49 +0200

    working on call back

--- RabbitMQTests/Exercise06RPC/Client.py
+++ RabbitMQTests/Exercise06RPC/Client.py
@@ -1,20 +1,29 @@
 import pika
 
-connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
-channel = connection.channel()
-print("Connection Created")
 
-result = channel.queue_declare(exclusive=True)
-callback_queue = result.method.queue
+class Fibonacci():
+    def __init__(self):
+        self.connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+        self.channel = connection.channel()
+        print("Connection Created")
 
-channel.basic_publish(exchange='',
-                      routing_key='rpc_queue',
-                      properties=pika.BasicProperties(
-                            reply_to = callback_queue,
-                            ),
-                      body=request)
+        self.result = channel.queue_declare(exclusive=True)     #Declare the callback queue
+        self.callback_queue = result.method.queue
+
+        self.channel.basic_publish(exchange='',
+                              routing_key='rpc_queue',
+                              properties=pika.BasicProperties(reply_to = callback_queue),
+                              body=request)
+
+
 '''
 We use a default exchange identified by an empty string. 
 This exchange is special: it allows us to specify exactly to which queue the message 
 should go. The queue name needs to be specified in the routing_key parameter:
 '''
+
+
+
+
+
+
