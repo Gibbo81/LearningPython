f59e0f152f4a59a84c823790fa2db445070e03d3
Giuliano Masi <masigiuliano@yahoo.it>
2016-07-16 17:34:29 +0200

    complete __slots__

--- PythonApplication1/32AdvancedClassTopic.py
+++ PythonApplication1/32AdvancedClassTopic.py
@@ -15,7 +15,7 @@ class B1: pass
 class C1(A1,B1): pass
 
 class Slot: #It"s possible to force a clas to only have a subset of attribute by defining them into __slot__
-    __slots__ = ['age','name','secondname','job']
+    __slots__ = ['age','name','secondname','job']   #class level attribute
 
 #We can still accommodate extra attributes, though, by including __dict__ explicitly in
 #__slots__, in order to create an attribute namespace dictionary too:
@@ -45,17 +45,21 @@ print("Class c1 OLD search pattern (NO diamond) --> C1.__mro__: ", C1.__mro__)
 print('------------------------------------------------------------')
 print("Working with __slot__")
 print('It"s possible to force a clas to only have a subset of attribute by defining them into __slot__')
-u = Slot()
-u.name = "bob"
-print('u.__slots__: ',u.__slots__)
-print("u.name: ",u.name)
+u1 = Slot()
+u2 = Slot()
+u1.name = "bob"
+u2.name = "sillas"
+print('u1.__slots__: ',u1.__slots__)
+print("u1.name: ",u1.name)
+print("u2.name: ",u2.name)
 #u.hair = 'red'  #AttributeError: 'Slot' object has no attribute 'hair' 
 
 print('If needed we can include __dict__ explicitly in __slots__,')
 sad = SlotAndDict(40,98)
 sad.fuffa = 76
 print("sad.__dict__: ",sad.__dict__)
-
+print("To see all the attribute")
+print("[x for x in dir(sad) if not x.startswith('__')]: " , [x for x in dir(sad) if not x.startswith('__')])
 print('------------------------------------------------------------')
 
 print('------------------------------------------------------------')
