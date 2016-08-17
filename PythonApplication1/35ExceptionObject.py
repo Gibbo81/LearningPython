c45dfe5726f8682b16f3cac3fe37fe9b8b65867f
Giuliano Masi <masigiuliano@yahoo.it>
2016-08-17 21:55:28 +0200

    exception hierarchies

--- PythonApplication1/35ExceptionObject.py
+++ PythonApplication1/35ExceptionObject.py
@@ -21,12 +21,23 @@ for x in (raise0, raise1, raise2):
         x()
     except pe.GeneralException as e:             #catches all the hierarchy of exception classes
         print("caught %s" % sys.exc_info()[0])   #gives information about the most recent exception
-                                                 #reaslly usefull with empty except where we cannot use as... 
+                                                 #really usefull with empty except where we cannot use as... 
         print("/*/*/*e.__class__: %s*/*/*/" % e.__class__)
 print('---------------------------------------------------------------------')
-
+print("Default printing and state")
+ex = TimeoutError(1,2,3,'a','b','c')
+print("TimeoutError(1,2,3,'a','b','c').args: %s" % str(ex.args))
+print("TimeoutError(1,2,3,'a','b','c'): %s" % ex)
+'''
+Unless you redefine the constructors your classes inherit from them, any constructor arguments you 
+pass to these classes are automatically saved in the instance's args tuple attribute, and are 
+automatically displayed when the instance is printed
+'''
 print('---------------------------------------------------------------------')
-
+print('It works also for personalized exception (no override)')
+ex = pe.SpecificException2(1,2,3,'a','b','c')
+print("pe.SpecificException2(1,2,3,'a','b','c').args: %s" % str(ex.args))
+print("pe.SpecificException2(1,2,3,'a','b','c'): %s" % ex)
 print('---------------------------------------------------------------------')
 
 print('---------------------------------------------------------------------')
