c8e5b4fd7e859c7b5e3553384d6f0f6082b95941
Giuliano Masi <masigiuliano@yahoo.it>
2017-02-12 18:38:49 +0100

    rename project

---
+++ ProgrammingPython/ProgrammingPython.py
@@ -0,0 +1,22 @@
+
+
+bob = ['bob smith', 42, 30000,'software']
+sue = ['sue Jones', 52, 50000,'hardware']
+
+peoples = [bob, sue]
+
+print(peoples[0][0].split()[-1])
+peoples[1][2] *= 1.25
+
+for x in peoples:
+    print(x)
+
+
+
+
+
+
+
+
+
+print('------------------------------------------------------------')
