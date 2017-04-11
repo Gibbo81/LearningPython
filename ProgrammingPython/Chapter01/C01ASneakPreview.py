f855aca08e1645b618f83bc650d9dcc446ce73d6
Giuliano Masi <masigiuliano@yahoo.it>
2017-04-11 22:31:00 +0200

    interactive user

--- ProgrammingPython/Chapter01/C01ASneakPreview.py
+++ ProgrammingPython/Chapter01/C01ASneakPreview.py
@@ -78,7 +78,45 @@ from Manager import Manager
 p1 = PersonRecord("Bob jr.", 23, 23000, "Doc");
 p2 = PersonRecord("Martin jr.", 57, 36000, "King");
 p3 = Manager("Rich sr.", 57, 36000);
-records = [p1, p2, p3]
+p4 = PersonRecord("pippo", 100, 100, "Cartton");
+records = [p1, p2, p3, p4]
 print(p1)
 print(p3)
+
+print('Write classes to file')
+with shelve.open('ClassShelve') as dbf:
+    for x in records:
+        dbf[x.name]=x
+
+print('Read classes from file')
+with shelve.open('ClassShelve') as dbf:
+    for x in dbf.keys():
+        print(dbf[x])
+    Rich = dbf["Rich sr."]
+    Rich.GiveRaise(0.1)
+    dbf["Rich sr."] = Rich  #Notice how we still fetch, update, and reassign to keys to update the shelve
+    print('------- More money to Rich sr. ---------')
+    print(dbf["Rich sr."])
+print('------------------------------------------------------------')
+
+print('Read data in interactive way')
+
+fieldnames = ('name', 'age', 'job', 'pay')
+with shelve.open('ClassShelve') as dbf:
+    while True:
+        key = input('Key? ')
+        if not key: break
+        try:
+            record = dbf[key]
+        except: 
+            print('Not Found')
+        else:
+            for f in fieldnames:
+                print(f, ':' , getattr(record,f))
+
+print('Stop reading data')
+
+
+
+
 
